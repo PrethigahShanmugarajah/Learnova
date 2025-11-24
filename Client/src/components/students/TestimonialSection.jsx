8e4bbc29ea17ec347d97658850d394fd4e734993
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-25 00:53:28 +0530

    Complete student TestimonialSection component

--- Client/src/components/students/TestimonialSection.jsx
+++ Client/src/components/students/TestimonialSection.jsx
@@ -1,10 +1,78 @@
 // Learnova / Client / src / components / students / TestimonialSection.jsx
 import React from "react";
+import { assets, dummyTestimonial } from "../../assets/assets";
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
 
 const TestimonialSection = () => {
   return (
-    <div>
-      <h1 className="text-green-500 bg-green-600">TestimonialSection</h1>
+    <div className="pb-14 px-8 md:px-0 bg-white">
+      <h2 className="text-3xl font-medium text-gray-800">Testimonials</h2>
+      <p className="md:text-base text-gray-500 mt-3">
+        {/* Hear from our learners as they share their journeys of transformation,
+        success, and how our <br /> platform has made a difference in their
+        lives. */}
+        Explore real stories of achievement and progress, and how our <br />
+        platform helped learners reach their goals.
+      </p>
+
+      <div className="grid grid-auto gap-8 mt-14">
+        {dummyTestimonial.map((testimonial, index) => (
+          <div
+            key={index}
+            className="text-sm text-left border border-gray-200 pb-6 rounded-lg bg-white shadow-[0px_4px_15px_0px] shadow-black/5 overflow-hidden"
+          >
+            <div className="flex items-center gap-4 px-5 py-4 bg-gray-500/10">
+              <img
+                className="h-12 w-12 rounded-full"
+                src={testimonial.image}
+                alt={testimonial.name}
+              />
+
+              <div>
+                <h1 className="text-lg font-medium text-gray-800">
+                  {testimonial.name}
+                </h1>
+
+                <p className="text-gray-800/80">{testimonial.role}</p>
+              </div>
+            </div>
+
+            <div className="p-5 pb-7">
+              <div className="flex gap-0.5">
+                {/* {[...Array(5)].map((_, i) => (
+                  <img
+                    className="h-5"
+                    key={i}
+                    src={
+                      i < Math.floor(testimonial.rating)
+                        ? assets.star
+                        : assets.star_blank
+                    }
+                    alt="Star"
+                  />
+                ))} */}
+
+                {[...Array(5)].map((_, i) =>
+                  i < Math.floor(testimonial.rating) ? (
+                    <AiFillStar key={i} className="h-5 w-5 text-yellow-500" />
+                  ) : (
+                    <AiOutlineStar
+                      key={i}
+                      className="h-5 w-5 text-yellow-500"
+                    />
+                  )
+                )}
+              </div>
+
+              <p className="text-gray-500 mt-5">{testimonial.feedback}</p>
+
+              <a href="#" className="text-blue-500 underline px-5">
+                Readmore
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
