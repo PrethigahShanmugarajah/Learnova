05f8c7fa8c907c2945cd495ad084efe9b37f67f5
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-24 22:35:55 +0530

    Complete Hero component

--- Client/src/components/students/Hero.jsx
+++ Client/src/components/students/Hero.jsx
@@ -1,10 +1,32 @@
 // Learnova / Client / src / components / students / Hero.jsx
 import React from "react";
+import { assets } from "../../assets/assets";
+import SearchBar from "./SearchBar";
 
 const Hero = () => {
   return (
-    <div>
-      <h1 className="text-green-500 bg-green-600">Hero</h1>
+    <div className="flex flex-col items-center justify-center w-full md:pt-36 pt-20 px-7 md:px-0 space-y-7 text-center bg-linear-to-b from-cyan-100/70">
+      <h1 className="home-heading-large md:home-heading-large relative font-bold text-gray-800 max-w-3xl mx-auto">
+        Take control of your future with courses carefully{" "}
+        <span className="text-green-600">tailored to your goals.</span>{" "}
+        {/* <img
+          src={assets.sketch}
+          alt="Sketch"
+          className="md:block hidden absolute -bottom-7 right-0"
+        /> */}
+      </h1>
+
+      <p className="md:block hidden text-gray-500 max-w-2xl mx-auto">
+        We connect you with world-class instructors, interactive courses, and a
+        supportive community to help you reach your personal and professional
+        goals.
+      </p>
+
+      <p className="md:hidden text-gray-500 max-w-sm mx-auto">
+        Learn from world-class instructors and achieve your professional goals.
+      </p>
+
+      <SearchBar />
     </div>
   );
 };
