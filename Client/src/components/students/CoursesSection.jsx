717a0ef61a7d58dec0591cbc55a7e29b424982d6
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-25 00:14:48 +0530

    Complete student CoursesSection component

--- Client/src/components/students/CoursesSection.jsx
+++ Client/src/components/students/CoursesSection.jsx
@@ -1,10 +1,47 @@
 // Learnova / Client / src / components / students / CoursesSection.jsx
-import React from "react";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AppContext } from "../../context/AppContext";
+import CourseCard from "./CourseCard";
+import Button from "../Button";
 
 const CoursesSection = () => {
+  const { navigate, currency, allCourses, setAllCourses } =
+    useContext(AppContext);
+
   return (
-    <div>
-      <h1 className="text-green-500 bg-green-600">CoursesSection</h1>
+    <div className="py-16 md:px-40 px-8 bg-slate-50">
+      <h2 className="text-3xl font-medium text-black">Learn from the best</h2>
+      <p className="text-sm md:text-base text-gray-500 mt-3">
+        Discover our top-rated courses across various categories. From coding
+        and design to <br /> business and wellness, our courses are crafted to
+        deliver results.
+      </p>
+
+      <div className="grid grid-auto px-4 md:px-0 md:my-16 my-10 gap-4">
+        {allCourses.slice(0, 4).map((course, index) => (
+          <CourseCard key={index} course={course} />
+        ))}
+      </div>
+
+      {/* <Link
+        to={"/course-list"}
+        onClick={() => scrollTo(0, 0)}
+        className="text-gray-500 border border-gray-200 px-10 py-3 rounded"
+      >
+        Show all courses
+      </Link> */}
+
+      <Button
+        onClick={() => {
+          navigate("/course-list");
+          scrollTo(0, 0);
+        }}
+        className="mt-4"
+        variant="secondary"
+      >
+        Show all courses
+      </Button>
     </div>
   );
 };
