f4a08a3cd04b7b4ab14b92d7bdc84c1fdf258eb1
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-24 23:13:33 +0530

    Update student Home page with Companies component

--- Client/src/pages/students/Home.jsx
+++ Client/src/pages/students/Home.jsx
@@ -1,11 +1,13 @@
 // Learnova / Client / src / pages / students / Home.jsx
 import React from "react";
 import Hero from "../../components/students/Hero";
+import Companies from "../../components/students/Companies";
 
 const Home = () => {
   return (
     <div className="flex flex-col items-center space-y-7 text-center">
       <Hero />
+      <Companies />
     </div>
   );
 };
