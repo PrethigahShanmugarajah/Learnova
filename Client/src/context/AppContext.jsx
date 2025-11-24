70bea60d5a2c58c7ed379f5043de152ae64326c5
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-25 00:06:21 +0530

    Update AppContext.jsx

--- Client/src/context/AppContext.jsx
+++ Client/src/context/AppContext.jsx
@@ -1,12 +1,47 @@
 //Learnoa / Client / src / context / AppContext.jsx
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { dummyCourses } from "../assets/assets";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
   const navigate = useNavigate();
-  const value = { navigate };
+  const currency = import.meta.env.VITE_CURRENCY;
+
+  const [allCourses, setAllCourses] = useState([]);
+  const [isEducator, setIsEducator] = useState(true);
+
+  // Fetch All Courses
+  const fetchAllCourses = async () => {
+    setAllCourses(dummyCourses);
+  };
+
+  // Function to calculate Average rating of course
+  const calculateRating = (course) => {
+    if (course.courseRatings.length === 0) {
+      return 0;
+    }
+    let totalRating = 0;
+    course.courseRatings.forEach((rating) => {
+      totalRating += rating.rating;
+    });
+    return totalRating / course.courseRatings.length;
+  };
+
+  useEffect(() => {
+    fetchAllCourses();
+  }, []);
+
+  const value = {
+    navigate,
+    currency,
+    allCourses,
+    setAllCourses,
+    calculateRating,
+    isEducator,
+    setIsEducator,
+  };
 
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
