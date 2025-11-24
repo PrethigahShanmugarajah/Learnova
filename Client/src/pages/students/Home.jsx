b04eea0abe67dd06f34c8c302769a4638226790e
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-25 02:13:32 +0530

    Update student Home page with Footer component  & Complete it

--- Client/src/pages/students/Home.jsx
+++ Client/src/pages/students/Home.jsx
@@ -5,15 +5,17 @@ import Companies from "../../components/students/Companies";
 import CoursesSection from "../../components/students/CoursesSection";
 import TestimonialSection from "../../components/students/TestimonialSection";
 import CallToAction from "../../components/students/CallToAction";
+import Footer from "../../components/students/Footer";
 
 const Home = () => {
   return (
-    <div className="flex flex-col items-center space-y-7 text-center">
+    <div className="flex flex-col items-center text-center">
       <Hero />
       <Companies />
       <CoursesSection />
       <TestimonialSection />
       <CallToAction />
+      <Footer />
     </div>
   );
 };
