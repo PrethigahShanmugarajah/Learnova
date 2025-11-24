75a02bdeefcfffc9888b0df76857702f7c23ede4
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-25 02:12:14 +0530

    Complete student Footer component

--- Client/src/components/students/Footer.jsx
+++ Client/src/components/students/Footer.jsx
@@ -1,11 +1,76 @@
 // Learnova / Client / src / components / students / Footer.jsx
 import React from "react";
+import { assets } from "../../assets/assets";
+import Button from "../Button";
 
 const Footer = () => {
   return (
-    <div>
-      <h1 className="text-green-500 bg-green-600">Footer</h1>
-    </div>
+    <footer className="bg-gray-900 md:px-36 text-left w-full">
+      <div className="flex flex-col md:flex-row items-start px-8 md:px-0 justify-center gap-10 md:gap-32 py-10 border-b border-gray-200">
+        <div className="flex flex-col md:items-start items-center w-full">
+          <img src={assets.Logo_Light} alt="Logo" className="w-28 lg:w-32" />
+
+          <p className="mt-6 text-center md:text-left text-sm text-white/80">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur
+            mollitia ut maiores vero sunt tenetur facilis veniam repellat hic
+            necessitatibus! Laborum, magnam voluptas recusandae repudiandae
+            nostrum culpa maxime dolorum sapiente!
+          </p>
+        </div>
+
+        <div className="flex flex-col md:items-start items-center w-full">
+          <h2 className="font-semibold text-white mb-5">Company</h2>
+          <ul className="flex md:flex-col w-full justify-between text-sm text-white/80 md:space-y-2">
+            <li>
+              <a href="#">Home</a>
+            </li>
+
+            <li>
+              <a href="#">About us</a>
+            </li>
+
+            <li>
+              <a href="#">Contact us</a>
+            </li>
+
+            <li>
+              <a href="#">Privacy policy</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="hidden md:flex flex-col items-start w-full">
+          <h2 className="font-semibold text-white mb-5">
+            Subscribe to our newsletter
+          </h2>
+
+          <p className="text-sm text-white/80">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia,
+            voluptate tempore.
+          </p>
+
+          <div className="flex items-center gap-2 pt-4">
+            <input
+              type="email"
+              placeholder="Enter your email"
+              className="border border-gray-200 bg-gray-800 text-gray-500 placeholder-gray-300 outline-none w-64 h-9 rounded px-2 text-sm"
+            />
+
+            <button className="bg-blue-600 w-24 h-9 text-white rounded">
+              Subscribe
+            </button>
+
+            <Button className="w-24 h-9 px-0! py-0!" variant={"primary"}>
+              Subscribe
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      <p className="py-4 text-center text-xs md:text-sm text-white/60">
+        Copyright 2025 & Learnova. All Right Reserved.
+      </p>
+    </footer>
   );
 };
 
