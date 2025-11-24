f85dde2fcad03fc6780ecd9d76f5a641fbdbee81
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-24 21:39:59 +0530

    Complete Button component

--- Client/src/components/Button.jsx
+++ Client/src/components/Button.jsx
@@ -1,11 +1,25 @@
 // Learnova / Client / src / components / Button.jsx
 import React from "react";
 
-const Button = () => {
+const Button = ({ text, children, onClick, className, type, variant }) => {
+  const variants = {
+    primary:
+      "bg-green-500 text-white border border-green-500 hover:bg-green-600",
+    secondary:
+      "bg-transparent text-black border border-white hover:bg-gray-900",
+    text: "bg-transparent text-black border-none hover:bg-transparent",
+  };
+
+  const baseClasses = `cursor-pointer px-6 py-2 rounded-md transition hover:rounded-full ${variants[variant]}`;
+
   return (
-    <div>
-      <h1 className="text-green-500 bg-green-600">Button</h1>
-    </div>
+    <button
+      type={type}
+      onClick={onClick}
+      className={`${baseClasses} ${className || ""}`}
+    >
+      {children || text}
+    </button>
   );
 };
 
