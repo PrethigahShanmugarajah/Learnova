927b8760ab0467c7d24f1e4f076b2b8ba74eaa68
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-26 00:07:51 +0530

    Setup MongoDB connection

---
+++ Server/configs/mongodb.js
@@ -0,0 +1,15 @@
+// Learnova / Server / configs / mongodb.js
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    mongoose.connection.on("connected", () =>
+      console.log("Database Connected")
+    );
+    await mongoose.connect(`${process.env.MONGODB_URI}/learnova`);
+  } catch (error) {
+    console.log("Database Connection Error:", error.message);
+  }
+};
+
+export default connectDB;
