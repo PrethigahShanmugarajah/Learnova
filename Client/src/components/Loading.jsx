8e692003a3a67cbbb335e3acfd81ca7daec802d3
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-25 15:57:30 +0530

    Update Loading component

--- Client/src/components/Loading.jsx
+++ Client/src/components/Loading.jsx
@@ -1,10 +1,10 @@
 // Learnova / Client / src / components / Loading.jsx
-import React from "react";
+import React, { useEffect } from "react";
 
 const Loading = () => {
   return (
-    <div>
-      <h1 className="text-green-500 bg-green-600">Loading</h1>
+    <div className="flex items-center justify-center h-[80vh]">
+      <div className="animate-spin rounded-full h-24 w-24 border-t-10 border-green-500 border-solid"></div>
     </div>
   );
 };
