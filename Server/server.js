4c35206ea635915407676d287f9c1b7cef223dd7
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-26 02:06:40 +0530

    Update server.js with educatorRouter

--- Server/server.js
+++ Server/server.js
@@ -4,6 +4,8 @@ import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/mongodb.js";
 import { clerkWebhooks } from "./controllers/webhooks.js";
+import educatorRouter from "./routes/educatorRoutes.js";
+import { clerkMiddleware } from "@clerk/express";
 
 /* -------- INITIALIZE EXPRESS -------- */
 const app = express();
@@ -13,10 +15,12 @@ await connectDB();
 
 /* -------- MIDDLEWARE CONFIGURATION -------- */
 app.use(cors());
+app.use(clerkMiddleware());
 
 /* -------- ROUTES -------- */
 app.get("/", (req, res) => res.send("API is Working!"));
 app.post("/clerk", express.json(), clerkWebhooks);
+app.use("/api/educator", express.json(), educatorRouter);
 
 /* -------- PORT -------- */
 const port = process.env.PORT || 5000;
