41809848a02ee4d0b2cec3193297d94857ae0e27
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-25 22:57:20 +0530

    Complete educator StudentsEnrolled page

--- Client/src/pages/educator/StudentsEnrolled.jsx
+++ Client/src/pages/educator/StudentsEnrolled.jsx
@@ -1,11 +1,86 @@
 // Learnova / Client / src / pages / educator / StudentsEnrolled.jsx
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { dummyStudentEnrolled } from "../../assets/assets";
+import Loading from "../../components/Loading";
+import { formatDDMMYYYYWithTimeAMPM } from "../../utils/date";
 
 const StudentsEnrolled = () => {
-  return (
-    <div>
-      <h1 className="text-green-500 bg-green-600">StudentsEnrolled</h1>
+  const [enrolledStudents, setEnrolledStudents] = useState(null);
+
+  const fetchEnrolledStudents = async () => {
+    setEnrolledStudents(dummyStudentEnrolled);
+  };
+
+  useEffect(() => {
+    fetchEnrolledStudents(dummyStudentEnrolled);
+  }, []);
+
+  return enrolledStudents ? (
+    <div className="min-h-screen flex flex-col items-start justify-between md:p-8 md:pb-0 p-4 pt-8 pb-0">
+      <div className="flex flex-col items-center max-w-4xl w-full overflow-hidden rounded-md bg-white border border-gray-200">
+        <table className="table-fixed md:table-auto w-full overflow-hidden pb-4">
+          <thead className="text-black border-b border-gray-200 text-sm text-left">
+            <tr>
+              <th className="px-4 py-3 font-semibold text-center hidden sm:table-cell">
+                #
+              </th>
+
+              <th className="px-4 py-3 font-semibold">Student Name</th>
+
+              <th className="px-4 py-3 font-semibold">Course Title</th>
+
+              <th className="px-4 py-3 font-semibold hidden sm:table-cell">
+                Date
+              </th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {enrolledStudents.map((item, index) => (
+              <tr key={index} className="border-b border-gray-200">
+                <td className="px-4 py-3 text-center hidden sm:table-cell">
+                  {index + 1}
+                </td>
+
+                <td className="md:px-4 px-2 py-3 flex items-center space-x-3">
+                  <img
+                    src={item.student.imageUrl}
+                    alt={item.student.name}
+                    className="w-9 h-9 rounded-full"
+                  />
+                </td>
+
+                <td className="px-4 py-3 truncate">{item.courseTitle}</td>
+
+                <td className="px-4 py-3 hidden sm:table-cell">
+                  {/* {new Date(item.purchaseDate).toLocaleDateString()} */}
+
+                  {/* {formatDDMMYYYYWithTimeAMPM(item.purchaseDate)} */}
+
+                  <div className="flex flex-col">
+                    <span className="font-medium">
+                      {
+                        formatDDMMYYYYWithTimeAMPM(item.purchaseDate).split(
+                          " "
+                        )[0]
+                      }{" "}
+                    </span>
+                    <span className="text-gray-600 text-xs">
+                      {formatDDMMYYYYWithTimeAMPM(item.purchaseDate)
+                        .split(" ")
+                        .slice(1)
+                        .join(" ")}{" "}
+                    </span>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
+  ) : (
+    <Loading />
   );
 };
 
