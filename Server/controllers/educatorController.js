2380d34a2b766da35d79c5f481fa7573e46cb550
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-26 02:05:22 +0530

    Create function to Update Role to Educator

---
+++ Server/controllers/educatorController.js
@@ -0,0 +1,23 @@
+// Learnova / Server / controllers / educatorController.js
+import { clerkClient } from "@clerk/express";
+
+/* -------- Update Role to Educator -------- */
+export const updateRoleEducator = async (req, res) => {
+  try {
+    const userId = req.auth.userId;
+
+    await clerkClient.users.updateUserMetadata(userId, {
+      publicMetadata: {
+        role: "educator",
+      },
+    });
+    res.json({ success: true, message: "You can publish a course now!" });
+  } catch (error) {
+    console.error("Update Role to Educator Error:", error.message);
+
+    return res.status(500).json({
+      success: false,
+      message: `Update Role to Educator Error: ${error.message}`,
+    });
+  }
+};
