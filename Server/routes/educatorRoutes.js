05a86e8b88cf9b267a8c3f2dc56874a066adcfa4
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-26 04:49:46 +0530

    Update educatorRoutes with getEnrolledStudentsData route

--- Server/routes/educatorRoutes.js
+++ Server/routes/educatorRoutes.js
@@ -4,6 +4,7 @@ import {
   addCourse,
   educatorDashboardData,
   getEducatorCourses,
+  getEnrolledStudentsData,
   updateRoleEducator,
 } from "../controllers/educatorController.js";
 import upload from "../configs/multer.js";
@@ -20,5 +21,10 @@ educatorRouter.post(
 );
 educatorRouter.get("/courses", protectEducator, getEducatorCourses);
 educatorRouter.get("/dashboard", protectEducator, educatorDashboardData);
+educatorRouter.get(
+  "/enrolled-students",
+  protectEducator,
+  getEnrolledStudentsData
+);
 
 export default educatorRouter;
