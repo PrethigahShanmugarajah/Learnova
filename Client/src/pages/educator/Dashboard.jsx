1c7bf56fa73d6e22cf3f2de37a59ed6ce7f0909f
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-25 22:26:25 +0530

    Complete educator Dashboard page

--- Client/src/pages/educator/Dashboard.jsx
+++ Client/src/pages/educator/Dashboard.jsx
@@ -1,11 +1,125 @@
 // Learnova / Client / src / pages / educator / Dashboard.jsx
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { AppContext } from "../../context/AppContext";
+import { assets, dummyDashboardData } from "../../assets/assets";
+import { FaMoneyBillWave, FaUsers } from "react-icons/fa";
+import { LuCalendarDays } from "react-icons/lu";
+import Loading from "../../components/Loading";
 
 const Dashboard = () => {
-  return (
-    <div>
-      <h1 className="text-green-500 bg-green-600">Dashboard</h1>
+  const {
+    navigate,
+    currency,
+    allCourses,
+    setAllCourses,
+    calculateRating,
+    isEducator,
+    setIsEducator,
+    calculateChapterTime,
+    calculateCourseDuration,
+    calculateNoOfLectures,
+    enrolledCourses,
+    setEnrolledCourses,
+    fetchUserEnrolledCourses,
+  } = useContext(AppContext);
+
+  const [dashboardData, setDashboardData] = useState(null);
+
+  const fetchDashboardData = async () => {
+    setDashboardData(dummyDashboardData);
+  };
+
+  useEffect(() => {
+    fetchDashboardData();
+  }, []);
+
+  return dashboardData ? (
+    <div className="min-h-screen  flex flex-col items-start justify-between gap-8 md:p-8 md:pb-0 p-4 pt-8 pb-0">
+      <div className="space-y-5">
+        <div className="flex flex-wrap gap-5 items-center">
+          <div className="flex items-center gap-3 shadow-card border border-green-100 bg-green-50 p-4 w-56 rounded-md">
+            {/* <img src={assets.patients_icon} alt="Patients_Icon" /> */}
+            <FaUsers className="size-12 text-green-500 bg-green-100 p-2 rounded-md" />
+
+            <div>
+              <p className="text-2xl font-medium text-black">
+                {dashboardData.enrolledStudentsData.length}
+              </p>
+
+              <p className="text-sm text-gray-500">Total Enrolments</p>
+            </div>
+          </div>
+
+          <div className="flex items-center gap-3 shadow-card border border-green-100 bg-green-50 p-4 w-56 rounded-md">
+            {/* <img src={assets.appointments_icon} alt="Appointments_Icon" /> */}
+            <LuCalendarDays className="size-12 text-green-500 bg-green-100 p-2 rounded-md" />
+
+            <div>
+              <p className="text-2xl font-medium text-black">
+                {dashboardData.totalCourses}
+              </p>
+
+              <p className="text-sm text-gray-500">Total Courses</p>
+            </div>
+          </div>
+
+          <div className="flex items-center gap-3 shadow-card border border-green-100 bg-green-50 p-4 w-56 rounded-md">
+            {/* <img src={assets.earning_icon} alt="Earning_Icon" /> */}
+            <FaMoneyBillWave className="size-12 text-green-500 bg-green-100 p-2 rounded-md" />
+
+            <div>
+              <p className="text-2xl font-medium text-black">
+                {currency} {dashboardData.totalEarnings}
+              </p>
+
+              <p className="text-sm text-gray-500">Total Earnings</p>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <h2 className="pb-4 text-lg font-medium">Latest Enrolments</h2>
+          <div className="flex flex-col items-center max-w-4xl w-full overflow-hidden rounded-md bg-white border border-gray-200">
+            <table className="table-fixed md:table-auto w-full overflow-hidden">
+              <thead className="text-black border-b border-gray-200 text-sm text-left ">
+                <tr>
+                  <th className="px-4 py-3 font-semibold text-center hidden sm:table-cell">
+                    #
+                  </th>
+
+                  <th className="px-4 py-3 font-semibold">Student Name</th>
+
+                  <th className="px-4 py-3 font-semibold">Course Title</th>
+                </tr>
+              </thead>
+
+              <tbody className="text-sm text-black">
+                {dashboardData.enrolledStudentsData.map((item, index) => (
+                  <tr key={index} className="border-b border-gray-200">
+                    <td className="px-4 py-3 text-center hidden sm:table-cell">
+                      {index + 1}
+                    </td>
+
+                    <td className="md:px-4 px-2 py-3 flex items-center space-x-3">
+                      <img
+                        src={item.student.imageUrl}
+                        alt="Profile"
+                        className="w-9 h-9 rounded-full"
+                      />
+                      <span className="truncate">{item.student.name}</span>
+                    </td>
+
+                    <td className="px-4 py-3 truncate">{item.courseTitle}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
+  ) : (
+    <Loading />
   );
 };
 
