821459dcca3b22b04c604cdd151c3e14937ce705
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-26 00:10:48 +0530

    Update server.js with MongoDB connection

---
+++ Server/server.js
@@ -0,0 +1,24 @@
+// Learnova / Server / server.js
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import connectDB from "./configs/mongodb.js";
+
+/* -------- INITIALIZE EXPRESS -------- */
+const app = express();
+
+/* -------- CONNECT TO DATABASE -------- */
+connectDB();
+
+/* -------- MIDDLEWARE CONFIGURATION -------- */
+app.use(cors());
+
+/* -------- ROUTES -------- */
+app.get("/", (req, res) => res.send("API is Working!"));
+
+/* -------- PORT -------- */
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
