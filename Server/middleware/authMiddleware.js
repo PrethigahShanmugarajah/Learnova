7d1dd1ff00f35297922796a5abb0032dbae41835
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-26 03:43:58 +0530

    Create authMiddleware to protect Educator routes

---
+++ Server/middleware/authMiddleware.js
@@ -0,0 +1,25 @@
+// Learnova / Server / middleware / authMiddleware.js
+import { clerkClient } from "@clerk/express";
+
+// Product Educator Routes
+export const protectEducator = async (req, res, next) => {
+  try {
+    const userId = req.auth.userId;
+    const response = await clerkClient.users.getUser(userId);
+
+    if (response.publicMetadata.role !== "educator") {
+      return res
+        .status(403)
+        .json({ success: false, message: "Unauthorized Access" });
+    }
+
+    next();
+  } catch (error) {
+    console.error("Product Educator Routes Error:", error.message);
+
+    return res.status(500).json({
+      success: false,
+      message: `Product Educator Routes Error: ${error.message}`,
+    });
+  }
+};
