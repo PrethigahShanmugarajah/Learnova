cb8f2bf768750e89a8dc701418057a0a49686da6
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-26 15:12:53 +0530

    Update courseRoutes with getCourseId route

--- Server/routes/courseRoutes.js
+++ Server/routes/courseRoutes.js
@@ -1,9 +1,10 @@
 // Server / routes / courseRoutes.js
 import express from "express";
-import { getAllCourse } from "../controllers/courseController.js";
+import { getAllCourse, getCourseId } from "../controllers/courseController.js";
 
 const courseRouter = express.Router();
 
 courseRouter.get("/all", getAllCourse);
+courseRouter.get("/:id", getCourseId);
 
 export default courseRouter;
