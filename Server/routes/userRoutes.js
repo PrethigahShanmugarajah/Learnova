2cbc87477f2bf3d0f5a793feddaa26973fd2167f
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-26 21:00:51 +0530

    Update userRoutes with addUserRating route

--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -1,6 +1,7 @@
 // Server / routes / userRoutes.js
 import express from "express";
 import {
+  addUserRating,
   getUserCourseProgress,
   getUserData,
   purchaseCourse,
@@ -15,5 +16,6 @@ userRouter.get("/enrolled-courses", userEnrolledCourses);
 userRouter.post("/purchase", purchaseCourse);
 userRouter.post("/update-course-progress", updateUserCourseProgress);
 userRouter.post("/get-course-progress", getUserCourseProgress);
+userRouter.post("/add-rating", addUserRating);
 
 export default userRouter;
