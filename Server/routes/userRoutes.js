61a7d905ec54d8848a2e2620c966b20a81066b2a
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-26 16:24:12 +0530

    Create userRoutes with getUserData route

---
+++ Server/routes/userRoutes.js
@@ -0,0 +1,9 @@
+// Server / routes / userRoutes.js
+import express from "express";
+import { getUserData } from "../controllers/userController.js";
+
+const userRouter = express.Router();
+
+userRouter.get("/data", getUserData);
+
+export default userRouter;
