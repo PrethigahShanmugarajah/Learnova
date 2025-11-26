4927f4223a5493ef97efa9e3b75db04e494d6801
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-26 18:10:19 +0530

    Create function to Purchase Course

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -1,5 +1,8 @@
 // Learnova / Server / controllers / userController.js.js
+import Course from "../models/Course.js";
+import Purchase from "../models/Purchase.js";
 import User from "../models/User.js";
+import Stripe from "stripe";
 
 /* -------- Get User Data -------- */
 export const getUserData = async (req, res) => {
@@ -42,6 +45,67 @@ export const userEnrolledCourses = async (req, res) => {
     return res.status(500).json({
       success: false,
       message: `Users Enrolled Courses With Lecture Links Error: ${error.message}`,
+    });
+  }
+};
+
+/* -------- Purchase Course -------- */
+export const purchaseCourse = async (req, res) => {
+  try {
+    const { courseId } = req.body;
+    const { origin } = req.headers;
+    const userId = req.auth.userId;
+    const userData = await User.findById(userId);
+    const courseData = await Course.findById(courseId);
+
+    if (!userData || !courseData) {
+      return res.json({ success: false, message: "Data not found" });
+    }
+
+    const purchaseData = {
+      courseId: courseData._id,
+      userId,
+      amount: (
+        courseData.coursePrice -
+        (courseData.discount * courseData.coursePrice) / 100
+      ).toFixed(2),
+    };
+
+    const newPurchase = await Purchase.create(purchaseData);
+
+    // Stripe Gateway Initialize
+    const stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+    const currency = process.env.CURRENCY.toLowerCase();
+
+    // Creating line items to for Stripe
+
+    const line_items = [
+      {
+        price_data: {
+          currency,
+          product_data: { name: courseData.courseTitle },
+          unit_amount: Math.floor(newPurchase.amount) * 100,
+        },
+        quantity: 1,
+      },
+    ];
+
+    const session = await stripeInstance.checkout.sessions.create({
+      success_url: `${origin}/loading/my-enrollments`,
+      cancel_url: `${origin}/`,
+      line_items: line_items,
+      mode: "payment",
+      metadata: { purchaseId: newPurchase._id.toString() },
+    });
+
+    res.json({ success: true, session_url: session.url });
+  } catch (error) {
+    console.error("Purchase Course Error:", error.message);
+
+    return res.status(500).json({
+      success: false,
+      message: `Purchase Course Error: ${error.message}`,
     });
   }
 };
