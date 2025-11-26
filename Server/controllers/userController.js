f8ed7109af402e162f67fa914d5df89c71b854b2
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-26 16:34:29 +0530

    Create function to get Users Enrolled Courses with Lecture Links

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -23,3 +23,23 @@ export const getUserData = async (req, res) => {
     });
   }
 };
+
+/* -------- Users Enrolled Courses With Lecture Links -------- */
+export const userEnrolledCourses = async (req, res) => {
+  try {
+    const userId = req.auth.userId;
+    const userData = await User.findById(userId).populate("enrolledCourses");
+
+    res.json({ success: true, enrolledCourses: userData.enrolledCourses });
+  } catch (error) {
+    console.error(
+      "Users Enrolled Courses With Lecture Links Error:",
+      error.message
+    );
+
+    return res.status(500).json({
+      success: false,
+      message: `Users Enrolled Courses With Lecture Links  Error: ${error.message}`,
+    });
+  }
+};
