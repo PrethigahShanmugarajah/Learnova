1904ad1e011eed6cb159f8685906b866d8879df6
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-26 18:11:06 +0530

    Update userRoutes with purchaseCourse route

--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -2,6 +2,7 @@
 import express from "express";
 import {
   getUserData,
+  purchaseCourse,
   userEnrolledCourses,
 } from "../controllers/userController.js";
 
@@ -9,5 +10,6 @@ const userRouter = express.Router();
 
 userRouter.get("/data", getUserData);
 userRouter.get("/enrolled-courses", userEnrolledCourses);
+userRouter.post("/purchase", purchaseCourse);
 
 export default userRouter;
