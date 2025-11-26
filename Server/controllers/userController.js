6c37005802e1e70e187157174bef0ae85e7cc25e
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-26 19:29:01 +0530

    Update userController.js

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -98,11 +98,7 @@ export const purchaseCourse = async (req, res) => {
       cancel_url: `${origin.replace(/\/$/, "")}/`,
       line_items: line_items,
       mode: "payment",
-      // metadata: { purchaseId: newPurchase._id.toString() },
-      metadata: {
-        purchaseId: newPurchase._id.toString(),
-        session_id: session.id,
-      },
+      metadata: { purchaseId: newPurchase._id.toString() },
     });
 
     res.json({ success: true, session_url: session.url });
