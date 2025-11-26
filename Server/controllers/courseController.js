117109d25329f3437e2928cba9bd3a42a88ec29d
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-26 15:12:48 +0530

    Create function to Get Course by ID

--- Server/controllers/courseController.js
+++ Server/controllers/courseController.js
@@ -18,3 +18,29 @@ export const getAllCourse = async (req, res) => {
     });
   }
 };
+
+/* -------- Get Course by ID -------- */
+export const getCourseId = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const courseData = await Course.findById(id).populate({ path: "educator" });
+
+    // Remove LecturelUrl if is PreviewFree is false
+    courseData.courseContent.forEach((chapter) => {
+      chapter.chapterContent.forEach((lecture) => {
+        if (!lecture.isPreviewFree) {
+          lecture.lecturelUrl = "";
+        }
+      });
+    });
+
+    return res.status(200).json({ success: true, courseData });
+  } catch (error) {
+    console.error("Get Course by ID Error:", error.message);
+
+    return res.status(500).json({
+      success: false,
+      message: `Get Course by ID Error: ${error.message}`,
+    });
+  }
+};
