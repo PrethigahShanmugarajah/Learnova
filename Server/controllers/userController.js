6002d7eec30291e391acb72b912a156697478385
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-26 19:00:57 +0530

    Fixed error in Purchase Course function

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -92,8 +92,10 @@ export const purchaseCourse = async (req, res) => {
     ];
 
     const session = await stripeInstance.checkout.sessions.create({
-      success_url: `${origin}/loading/my-enrollments`,
-      cancel_url: `${origin}/`,
+      // success_url: `${origin}/loading/my-enrollments`,
+      // cancel_url: `${origin}/`,
+      success_url: `${origin.replace(/\/$/, "")}/loading/my-enrollments`,
+      cancel_url: `${origin.replace(/\/$/, "")}/`,
       line_items: line_items,
       mode: "payment",
       metadata: { purchaseId: newPurchase._id.toString() },
